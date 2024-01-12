5217a818dfd3395da441335036eb518f3f34068f
동영 <57888678+dahli4@users.noreply.github.com>
2024-01-12 10:06:27 +0900

    [level Lv.1] Title: 부족한 금액 계산하기, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/Lv.1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.swift
+++ 프로그래머스/Lv.1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.swift
@@ -6,10 +6,12 @@ func solution(_ price:Int, _ money:Int, _ count:Int) -> Int64{
     for i in 1...count {
         countMoney += price * i
     }
-    if countMoney > money{
-        return Int64(countMoney - money)
-    }
-    else {
-        return 0
-    }
+    
+    // if countMoney > money{
+    //     return Int64(countMoney - money)
+    // }
+    // else {
+    //     return 0
+    // }
+    return Int64(countMoney > money ? countMoney - money : 0)
 }
